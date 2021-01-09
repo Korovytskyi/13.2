886e2459b07af3c634a6bf9204f98055184b97a7
Taras <taras.korovytskyi.itis.2020@lpnu.ua>
2021-01-09 15:58:15 +0200

    13.2

---
+++ 13.2/13.2.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <corecrt_math.h>
+#include <iostream>
+
+#define P z
+int main()
+
+{
+	int(z);
+	int(x);
+	int(y);
+	
+	double(n1);
+	double(n2);
+	double(n3);
+	double(n4);
+	std::cout << ("z= "); std::cin >> (z);
+	std::cout << ("x= "); std::cin >> (x);
+	std::cout << ("y= "); std::cin >> (y);
+	
+	n1 = (abs(x)+abs(y)+abs(z));
+	n2 = (x*x*x);
+	n3 = ((2*x)+y);
+	n4 = (((2*y)-2)* ((2 * y) - 2));
+	
+#if P==2
+printf(n1";"n2);
+	
+	
+
+#else
+	
+	printf("8" ";" "1");
+#endif
+	getchar();
+	return 0;
+}
